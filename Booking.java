8718b187f9f3e76a62f76394e567ca9dc7bd823f
ArvinP <Arvinpourabdollah@gmail.com>
2025-08-13 14:14:07 +1000

    Customer & Booking basic structure compelete

--- Booking.java
+++ Booking.java
@@ -1,4 +1,81 @@
 public class Booking {
+
+    private String customerName;
+    private String customerEmail;
+    private String selectedProperty;
+    private String propertyHostName;
+    private int numOfGuests;
+    private int stayDuration;
+    private double totalAmount;
+    
+
+    public Booking(String customerName, String customerEmail, String selectedProperty, String propertyHostName, 
+            int numOfGuests, int stayDuration, double totalAmount){
+
+                this.customerName = customerName;
+                this.customerEmail = customerEmail;
+                this.selectedProperty = selectedProperty;
+                this.propertyHostName = propertyHostName;
+                this.numOfGuests = numOfGuests;
+                this.stayDuration = stayDuration;
+                this.totalAmount = totalAmount;
+    }
+
+
+    public String getCustomerName() {        return customerName;    }
+
+
+    public void setCustomerName(String customerName) {        this.customerName = customerName;    }
+
+
+    public String getCustomerEmail() {        return customerEmail;    }
+
+
+    public void setCustomerEmail(String customerEmail) {        this.customerEmail = customerEmail;    }
+
+
+    public String getSelectedProperty() {        return selectedProperty;    }
+
+
+    public void setSelectedProperty(String selectedProperty) {        this.selectedProperty = selectedProperty;    }
+
+
+    public String getPropertyHostName() {        return propertyHostName;    }
+
+
+    public void setPropertyHostName(String propertyHostName) {        this.propertyHostName = propertyHostName;    }
+
+
+    public int getNumOfGuests() {        return numOfGuests;    }
+
+
+    public void setNumOfGuests(int numOfGuests) {        this.numOfGuests = numOfGuests;    }
+
+
+    public int getStayDuration() {        return stayDuration;    }
+
+
+    public void setStayDuration(int stayDuration) {        this.stayDuration = stayDuration;    }
+
+
+    public double getTotalAmount() {        return totalAmount;    }
+
+
+    public void setTotalAmount(double totalAmount) {        this.totalAmount = totalAmount;    }
+
+
+    @Override
+    public String toString() {
+        return "Booking" +
+                "Name: " + customerName + 
+                "Email: " + customerEmail + 
+                "Your Stay: " + selectedProperty + 
+                "Your Host" + propertyHostName + 
+                "Number Of Guests: " + numOfGuests + 
+                "Duration Of Stay: " + stayDuration + 
+                ",Total Paid: " + totalAmount;
+    }
+
     // The program will ask the user to reserve the property.
     // The user will be asked to provide check-in and checkout dates.
     // the program will print the information of the selected property
--- Customer.java
+++ Customer.java
@@ -1,8 +1,42 @@
 
 public class Customer {
     private String name;
-    private String sureName;
+    private String surName;
     private String eMail;
     private int numOfGuests;
     private int stayDuration;
+
+    public Customer(String name, String surName, String eMail, int numOfGuests, int stayDuration){//i will have a static methid and i will call that to calculate the stay duration
+        this.name = name;
+        this.surName = surName;
+        this.eMail = eMail;
+        this.numOfGuests = numOfGuests;
+        this.stayDuration = stayDuration;
+    }
+
+    public String getName() {        return name;    }
+
+    public void setName(String name) {        this.name = name;    }
+
+    public String getSurName() {        return surName;    }
+
+    public void setSurName(String surName) {        this.surName = surName;    }
+
+    public String geteMail() {        return eMail;    }
+
+    public void seteMail(String eMail) {        this.eMail = eMail;    }
+
+    public int getNumOfGuests() {        return numOfGuests;    }
+
+    public void setNumOfGuests(int numOfGuests) {        this.numOfGuests = numOfGuests;    }
+
+    public int getStayDuration() {        return stayDuration;    }
+
+    public void setStayDuration(int stayDuration) {        this.stayDuration = stayDuration;    }
+
+    @Override
+    public String toString() {
+        return "Customer [name=" + name + ", surName=" + surName + ", eMail=" + eMail + ", numOfGuests=" + numOfGuests
+                + ", stayDuration=" + stayDuration + "]";
+    }    
 }
